f9c4350766b176f376a71fdcff1a352a6823f23a
SamTomashi <samtomashikayembi@yahoo.fr>
2023-10-17 09:22:43 +0300

    implemented the code challenge

--- src/app.js
+++ src/app.js
@@ -1,22 +1,11 @@
-/**
- * Steps:
- * 1. Install and test json-server:done
- * 2. Fetch the data from our DB:done
- * 3. Display the data:done
- * 4. Implement an onclick listener:
- * 5. Display the details of the selected character
- * 6. Vote for a particular character
- */
 let chartacters = []
+
 document.addEventListener("DOMContentLoaded", function(){
-    getCharacters()
+    getCharcters()
 })
 
 
-
-
-
-function getCharacters(){
+function getCharcters(){
     fetch("http://localhost:3000/characters", {
         method: "GET",
         headers:{
@@ -29,33 +18,31 @@ function getCharacters(){
     })
 }
 
-
-//displaying the characters on the front-end
-function displayCharacters(characters){
+function displayCharacters(characters) {
     const characterbar = document.querySelector("#character-bar")
     for(character of characters){
         const span = document.createElement("span"); //create an element for each character
         span.innerText = character.name;
         span.setAttribute("id", character.id)
-    
-        span.addEventListener("click", (event)=> {
-            diaplayCharacterDetails(getCharacterById(characters, parseInt(event.target.id)))
-        });
+
+        span.addEventListener("click", (event)=>{
+            const character = getCharacterById(characters, parseInt(event.target.id));
+            diaplayCharacterDetails(character)
+
+        })
 
         characterbar.appendChild(span);
 
-        
     }
 }
-
 function diaplayCharacterDetails(character){
     const image = document.querySelector("#image");
     image.src = character.image
+    // console.log(image.src)
 }
 
 function getCharacterById(characters, id){
     return characters.find((character)=>{ 
         return character.id === id
-})
-
+    })
 }
